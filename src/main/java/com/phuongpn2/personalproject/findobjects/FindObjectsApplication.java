31988ea454e4eeed061f1ad678251bc328836955
Phạm Nguyên Phương <phamnguyenphuong@hotmail.com>
2023-07-23 22:56:21 +0700

    first commit

---
+++ src/main/java/com/phuongpn2/personalproject/findobjects/FindObjectsApplication.java
@@ -0,0 +1,13 @@
+package com.phuongpn2.personalproject.findobjects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindObjectsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FindObjectsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/phuongpn2/personalproject/findobjects/FindObjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phuongpn2.personalproject.findobjects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindObjectsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
